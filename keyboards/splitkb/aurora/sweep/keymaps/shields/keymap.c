55c5c7ae138c38a40bb109cd79f4a519f52e5995
Daniel Shields <daniel.shields@dmgmori.com>
2026-02-09 22:17:11 +0100

    Had to fallback on backspade on left thumb :/ causes too many problems when typing quick

--- keyboards/splitkb/aurora/sweep/keymaps/shields/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/shields/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_Z,      KC_X,      KC_C,      KC_D,      KC_V,       KC_K,      KC_H,      KC_COMM,   KC_DOT,    KC_ENT,
         //╰──────────┴──────────┴──────────┼──────────┼──────────┤├──────────┼──────────┼──────────┴──────────┴──────────╯
         //                                 │ LOWER    │  CTLR    ││  SHIFT   │SPC(RAISE)│
-                                            BSPC_LOWER, OSM_CTL,    OSM_SFT,   SPC_RAISE
+                                            MO(_LOWER), OSM_CTL,    OSM_SFT,   SPC_RAISE
         //                                 ╰──────────┴──────────╯╰──────────┴──────────╯
     ),
     [_LOWER] = LAYOUT(
